b8cd69b90f1e04d179520e163306cf75597de8bb
Arun <kkumararun.87@gmail.com>
2016-10-15 10:07:32 +0530

    day two

--- myecomfrontend/src/main/java/myecomfrontend/HelloController.java
+++ myecomfrontend/src/main/java/myecomfrontend/HelloController.java
@@ -7,30 +7,28 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
 
-	@RequestMapping(value={"/welcome","/"})
-public ModelAndView helloWorld(){
-	
-	ModelAndView model=new ModelAndView("index");
-	model.addObject("msg","Welcome to my page");
-	return model;
-}
-	
-	
+	@RequestMapping(value = { "/welcome", "/" })
+	public ModelAndView helloWorld() {
+
+		ModelAndView model = new ModelAndView("index");
+		model.addObject("msg", "Welcome to my page");
+		return model;
+	}
+
 	@RequestMapping("/aboutUs")
-	public ModelAndView aboutUsView(){
-		
-		ModelAndView model=new ModelAndView("About_Us");
-		model.addObject("msg","Welcome to our about us");
+	public ModelAndView aboutUsView() {
+
+		ModelAndView model = new ModelAndView("About_Us");
+		model.addObject("msg", "Welcome to our about us");
 		return model;
 	}
 
 	@RequestMapping("/contact")
-	public ModelAndView contactView(){
-		
-		ModelAndView model=new ModelAndView("contact");
-		model.addObject("msg","NIIT Raj Nagar");
+	public ModelAndView contactView() {
+
+		ModelAndView model = new ModelAndView("contact");
+		model.addObject("msg", "NIIT Raj Nagar");
 		return model;
 	}
-
 
 }
